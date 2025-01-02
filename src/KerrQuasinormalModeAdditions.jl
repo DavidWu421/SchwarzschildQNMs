22654a7a8481c41c59e4dae7cdb0d1bc86996e10
David Wu <dwuuu.421@gmail.com>
2025-01-02 11:49:37 -0500

    Add is_conjugate attribute

--- src/KerrQuasinormalModeAdditions.jl
+++ src/KerrQuasinormalModeAdditions.jl
@@ -7,7 +7,7 @@ function __init__()
     qnm.download_data()
 end
 
-function qnmfunctionnew(s,l,m,n,a; qnm=qnm,modesign="plus")
+function qnmfunctionnew(s,l,m,n,a; qnm=qnm,modesign="plus",is_conjugate=false)
     # qnmfunction defined on line 202 of KerrQuasinormalModes.jl/src/ModeFunctionInterface/Interface.jl
     # m should be the m of the plus mode, regardless of if you want the plus or minus mode
     grav_freq = qnm.modes_cache(s=s,l=l,m=m,n=n)
@@ -19,5 +19,5 @@ function qnmfunctionnew(s,l,m,n,a; qnm=qnm,modesign="plus")
         Cllʼ=[i % 2 == 0 ? -x : x for (i, x) in enumerate(Cllʼ)]
         Cllʼ=(-1)^l*conj.(Cllʼ)
     end
-    qnmfunction(Custom; s=s,l=l,m=m,n=n,a=a,ω=ω,Alm=Alm,Cllʼ=Cllʼ)
+    qnmfunction(Custom; s=s,l=l,m=m,n=n,a=a,ω=ω,Alm=Alm,Cllʼ=Cllʼ,is_conjugate=is_conjugate)
 end
