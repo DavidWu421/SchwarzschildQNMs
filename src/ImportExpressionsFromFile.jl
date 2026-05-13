f87d8acee9324fedd5cb830ce690844faf59248a
David Wu <dwuuu.421@gmail.com>
2026-05-13 10:07:21 -0500

    Initial changes to moving to NP quantities

--- src/ImportExpressionsFromFile.jl
+++ src/ImportExpressionsFromFile.jl
@@ -5,9 +5,16 @@ function GetExprMetric(file)
     exprs = []
     for (d,f) ∈ eachrow(df[df.Coefficient .!= "0",[:Index,:Coefficient]])
         # println((d,f))
-        d = d == "-" ? "" : d
-        d = replace(d,"th"=>"θ")
-        f = replace(f,"omega"=>"ω")
+        # d = d == "-" ? "" : d
+        # d = replace(d,"th"=>"θ")
+        f = replace(f,
+            "omega" => "ω",
+            "rho"   => "ρ",
+            "beta"  => "β",
+            "tau"   => "τ",
+            "mu"    => "μ",
+            "xi"    => "ξ"
+        )
         ps = "ψ"*d
         push!(exprs,"($f)*$(ps)(r,x)")
     end
@@ -18,7 +25,7 @@ end
 
 function MakeOp(file)
     thisexpr = GetExprMetric(file)
-    Op = eval(Meta.parse("((r,x,a,m,ω,s,ψ00,ψ01,ψ02,ψ03,ψ04,ψ05,ψ06,ψ10,ψ11,ψ12,ψ13,ψ14,ψ15,ψ16,ψ20,ψ21,ψ22,ψ23,ψ24,ψ25,ψ26,ψ30,ψ31,ψ32,ψ33,ψ34,ψ35,ψ36,ψ40,ψ41,ψ42,ψ43,ψ44,ψ45,ψ46,ψ50,ψ51,ψ52,ψ53,ψ54,ψ55,ψ56,ψ60,ψ61,ψ62,ψ63,ψ64,ψ65,ψ66; pertparam=0, M=1) -> $thisexpr)"))
+    Op = eval(Meta.parse("((r,x,a,m,ω,s,ψ00,ψ01,ψ10,ψ11; pertparam=0, M=1) -> $thisexpr)"))
     # println(thisexpr)
     Op
 end
---
+++ test/orderreducetest.jl
@@ -0,0 +1,60 @@
+using ContourIntegrals
+using KerrQNMShifts
+using KerrQuasinormalModes
+using Test
+using CSV, DataFrames
+
+println("Done usings")
+
+@testset "ScalarKerrPertToSchw" begin   #CHECKED AND GOOD
+
+    dwOfile = "./OperatorShifts/OrderReducedShifts/dwscalarOcoefficients.csv"
+    dϵOfile = "./OperatorShifts/OrderReducedShifts/δscalarOcoefficients.csv"
+
+    ∂ϵO = OperatorShift(dϵOfile)
+    ∂ωO = OperatorShift(dwOfile)
+
+    println("Made operator shifts")
+    
+    ψ = qnmfunctionnew(0,10,10,0,0.)
+
+    # Compile ψ
+    ψ(1,.5)
+    println("Past ψ compile")
+
+    ## Define the useful contours
+    r₊ = ψ.R.r₊ ; r₋ = ψ.R.r₋ ; s = ψ.s ; Δr = 0.1*(r₊-r₋); ϵ = eps(0.1);
+
+    # Define the Weight
+    Σ = let a= ψ.a
+        (r,z) -> Complex(r)^2+a^2*z^2
+    end
+    Δ = let a= ψ.a
+        (r,z) -> Complex(r)^2+a^2-2*r
+    end
+    weight = let a= ψ.a
+        (r,z) ->Σ(r,z)
+    end
+
+    #The upwards pointing contour
+    point1up = r₊ + Δr - Δr*im
+    point2up = r₊ - Δr - Δr*im
+    radial1up = SemiInfiniteLine(point1up , point1up + Δr*im , false)
+    angular = LineSegment(-1.0+100*ϵ , 1.0-100*ϵ , true) #to avoid the NaNs at the edges
+    C1up = radial1up ⊗ angular
+    radial2up = LineSegment(point1up,point2up,true)
+    C2up = radial2up ⊗ angular
+    radial3up = SemiInfiniteLine(point2up , point2up + Δr*im , true)
+    C3up = radial3up ⊗ angular
+    TheContourup = C1up⊕C2up⊕C3up
+
+    # Make the operators
+    ∂ϵOm2 = OperatorSandwich(ψ,∂ϵO,weight,ψ).Op
+    ∂ωOm2 = OperatorSandwich(ψ,∂ωO,weight,ψ).Op
+    println("Made Operators")
+
+    ∂ϵ𝒪m2 = Integrate(∂ϵOm2, TheContourup,abstol=1e-10)[1]
+    ∂ω𝒪m2 = Integrate(∂ωOm2, TheContourup,abstol=1e-10)[1]
+
+    @show δω = -(∂ϵ𝒪m2/∂ω𝒪m2)
+end
